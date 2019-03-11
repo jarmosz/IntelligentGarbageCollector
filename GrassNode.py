9746b930df125093591331925503c5aa3b4a226c
PawełStelmach <pasin265@gmail.com>
2019-03-11 18:33:03 +0100

    fixed reverted positions on x and y, added road sprites and it's render

--- GrassNode.py
+++ GrassNode.py
@@ -1,9 +1,12 @@
 from Node import Node
-class GrassNode(Node):
-    cell_size = 0
-    image = 1 # here goes sprite to display just for now let's set it to 1
-    IMAGE_1 = 1 # here goes first image .. e.g. IMAGE_2  = 'grass.jpg'
 
-    def __init__(self,position):
-        pass # randomize grass sprite
+
+class GrassNode(Node):
+    images
+
+    def __init__(self, position):
+        pass  # randomize grass sprite
         cell_size = Node.cell_size
+        self.
+
+    def random_grass_sprites():
--- Node.py
+++ Node.py
@@ -1,5 +1,4 @@
 class Node:
-    cell_size = 25
 
     def __init__(self, position):
         self.position = position
