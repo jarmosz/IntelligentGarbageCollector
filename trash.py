96234efc8f41b264821fcfa1377ff4838d46c6d3
Wojciech Jarmosz <jarmosz.wojciech@gmail.com>
2019-03-28 11:45:48 +0100

    Refactoring objects attribute name from node_type to type

--- trash.py
+++ trash.py
@@ -3,27 +3,27 @@ import random
 
 class Trash:
 
-    def __init__(self, node_type):
-        self.node_type = node_type
+    def __init__(self, type):
+        self.type = type
         self.texture = ''
         self.set_texture()
         self.capacity = randint(1, 1000)
     
     def set_texture(self):
-        if self.node_type == "empty_trash":
+        if self.type == "empty_trash":
             self.texture = 'sprites/empty.png'
-        elif self.node_type == "yellow_trash":
+        elif self.type == "yellow_trash":
             self.texture = 'sprites/yellow.png'
-        elif self.node_type == "blue_trash":
+        elif self.type == "blue_trash":
             self.texture = 'sprites/blue.jpg'
-        elif self.node_type == "red_trash":
+        elif self.type == "red_trash":
             self.texture = 'sprites/red.png'
     
     def get_texture(self):
         return self.texture
 
-    def get_node_type(self):
-        return self.node_type
+    def get_type(self):
+        return self.type
 
     def get_capacity(self):
         return self.capacity
