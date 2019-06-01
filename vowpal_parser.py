425eab4ddebc6123d7e92a2792aef6fc40eb2190
salwin <salwinwwy@gmail.com>
2019-06-01 21:52:14 +0200

    get data from x number of maps

--- vowpal_parser.py
+++ vowpal_parser.py
@@ -6,29 +6,30 @@ from itertools import groupby
 # 0 - grass 1 - road 2 - trash empty 3 - trash full
 class VowpalParser:
     SQUARE_SIZE = 5
-    MAP_RESOLUTION = 40
+    MAP_RESOLUTION = 30
+    NUMBER_OF_MAPS = 10
     def collect_data(self):
         f = open("vowpal_data.txt", "w")
-        _map = map.Map(self.MAP_RESOLUTION)
-        _truck = truck.Truck(_map)
-        _truck.set_current_map_state(_map.get_grid())
-        _map.set_truck_current_position_on_the_grid(_truck)
-        map_numerical = _map.get_grid_numerical()
-        move_list = [(_truck.current_position_x, _truck.current_position_y)]
-        move_list += BreathFirstSearch().start_bfs(_map, 'yellow_trash')
-        print(move_list)
-        current = move_list[0]
-        for i in range(1, len(move_list)):
-            if move_list[i] == 'collect':
-                square_state = self.get_grid_square(map_numerical, current)
-                move = 'c'                
-            else:
-                square_state = self.get_grid_square(map_numerical, move_list[i])
-                move = str(self.parse_move(current, move_list[i]))
-                current = move_list[i]
-            state = ' '.join(' '.join(str(x) for x in row) for row in square_state)            
-            f.write("{}|{}\n".format(str(move), state))
-            square_state = self.empty_trash(square_state)
+        for j in range(self.NUMBER_OF_MAPS):
+            _map = map.Map(self.MAP_RESOLUTION)
+            _truck = truck.Truck(_map)
+            _truck.set_current_map_state(_map.get_grid())
+            _map.set_truck_current_position_on_the_grid(_truck)
+            map_numerical = _map.get_grid_numerical()
+            move_list = [(_truck.current_position_x, _truck.current_position_y)]
+            move_list += BreathFirstSearch().start_bfs(_map, 'yellow_trash')
+            current = move_list[0]
+            for i in range(1, len(move_list)):
+                if move_list[i] == 'collect':
+                    square_state = self.get_grid_square(map_numerical, current)
+                    move = 'c'                
+                else:
+                    square_state = self.get_grid_square(map_numerical, move_list[i])
+                    move = str(self.parse_move(current, move_list[i]))
+                    current = move_list[i]
+                state = ' '.join(' '.join(str(x) for x in row) for row in square_state)            
+                f.write("{}|{}\n".format(str(move), state))
+                square_state = self.empty_trash(square_state)
         f.close()
         while(True):
             _map.update_window()
